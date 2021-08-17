a86ea238f44228deb11d08b6573d70a50606aed1
Luc Blassel <luc.blassel@gmail.com>
2021-08-17 03:57:01 +0200

    debugging postcss

--- themes/hagan/assets/css/postcss.config.js
+++ themes/hagan/assets/css/postcss.config.js
@@ -1,11 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ './hugo_stats.json' ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
-});
-
 module.exports = {
      plugins: [
          require("tailwindcss"),
--- themes/hagan/assets/css/tailwind.config.js
+++ themes/hagan/assets/css/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     theme: {
       extend: {}
     },
-    // darkMode: 'media',
+    darkMode: 'media',
     variants: {},
     plugins: [],
     purge: {
